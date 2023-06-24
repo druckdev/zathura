c8aeef9c9bc06e6b8a4b7df6233fb02c604824d5
Julian Prein <druckdev@protonmail.com>
2023-06-24 11:16:14 +0200

    TODO: Reset position if search fails w/ inc-search

--- zathura/shortcuts.c
+++ zathura/shortcuts.c
@@ -980,6 +980,7 @@ bool sc_search(girara_session_t* session, girara_argument_t* argument, girara_ev
       pos_x += (center_x - (double)cell_width / 2) / (double)doc_width;
     }
 
+    // TODO: jump back if search fails with incremental-search
     /* move to position */
     zathura_jumplist_add(zathura);
     position_set(zathura, pos_x, pos_y);
